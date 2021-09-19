3296cfc4d615c09887277adf74de95ff53540694
wxvirus <1418667580@qq.com>
2021-09-19 23:28:10 +0800

    gin框架如何使用重定向的操作

--- main.go
+++ main.go
@@ -45,6 +45,29 @@ func main() {
 		})
 	})
 
+	// 重定向
+	r.GET("/ready/index", func(c *gin.Context) {
+		//c.JSON(http.StatusOK, gin.H{
+		//	"status": "ok",
+		//})
+
+		// 重定向
+		c.Redirect(http.StatusMovedPermanently, "https://www.sogou.com")
+	})
+
+	// 路由重定向
+	r.GET("/a", func(c *gin.Context) {
+		// 跳转到b对应的路由函数
+		c.Request.URL.Path = "/b" // 把请求的URI地址修改
+		r.HandleContext(c)        // 继续后续的处理
+	})
+
+	r.GET("/b", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "bbbb",
+		})
+	})
+
 	r.GET("/upload", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "upload/index.tmpl", nil)
 	})
